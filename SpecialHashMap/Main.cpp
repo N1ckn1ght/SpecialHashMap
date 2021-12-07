4aaafd488441f3faca0af0f8bd8d9cf8c33f9c95
N1ckn1ght <50321432+N1ckn1ght@users.noreply.github.com>
2021-12-07 23:47:38 +0800

    Add files via upload

--- SpecialHashMap/Main.cpp
+++ SpecialHashMap/Main.cpp
@@ -3,5 +3,9 @@
 
 int main()
 {
-
+	SpecialHashMap<std::string> map;
+	map["(5, 2)"] = "value1";
+	map["(3, 4)"] = "value2";
+	map.toSortedSet();
+	std::cout << map.iloc(0) << "\n" << map.ploc(">3 <3") << "\n";
 }
